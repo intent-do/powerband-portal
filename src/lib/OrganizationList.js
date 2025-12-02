b3f48fdaf5559d8cba5b3341f04024fe21bc01f9
marrryy-intent <mary@intent.do>
2025-12-02 15:13:18 +0700

    changing to client type

--- src/lib/OrganizationList.js
+++ src/lib/OrganizationList.js
@@ -1,16 +1,19 @@
 import { connectDB, sql } from "../lib/db";
 
 export default async function OrganizationList() {
-    try {
-        const pool = await connectDB();
+  try {
+    const pool = await connectDB();
 
-        // const result = await pool.request().query(`select DISTINCT LinkOrgName as clientname from ArofloClient vatcr where LinkOrgName is not null order by LinkOrgName asc`);
-        const result = await pool.request().query(`SELECT DISTINCT ISNULL(NULLIF(ParentClient,''), ClientName) as clientname FROM [dbo].[ArofloParentChildClient] WHERE ClientType = 'RE Agents - Resi'`);
-
-        return result?.recordset;
-
-    } catch (error) {
-        console.error("Error fetching tasks:", error);
-        return error;
-    }
+    // const result = await pool.request().query(`select DISTINCT LinkOrgName as clientname from ArofloClient vatcr where LinkOrgName is not null order by LinkOrgName asc`);
+    // const result = await pool.request().query(`SELECT DISTINCT ISNULL(NULLIF(ParentClient,''), ClientName) as clientname FROM [dbo].[ArofloParentChildClient] WHERE ClientType = 'RE Agents - Resi'`);
+    const result = await pool
+      .request()
+      .query(
+        `SELECT DISTINCT ClientName as clientname FROM [dbo].[ArofloParentChildClient] WHERE ClientType = 'Businesses - Builders'`
+      );
+    return result?.recordset;
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    return error;
+  }
 }
